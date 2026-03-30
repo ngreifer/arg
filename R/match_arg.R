4ab4ba84ec6f6c6bc817becea067b42e8fb834de
Noah Greifer <noah.greifer@gmail.com>
2026-03-30 11:49:25 -0400

    Added call and support for preferential ignore case

--- R/match_arg.R
+++ R/match_arg.R
@@ -8,16 +8,18 @@
 #' @param x a string (or character vector if `several.ok = TRUE`) to match against `choices`. If `NULL`, the first element of `choices` is returned.
 #' @param choices a character vector of valid values.
 #' @param several.ok `logical`; if `TRUE`, `x` may contain more than one element. Defaults to `FALSE`.
-#' @param ignore.case `logical`; if `FALSE` (the default), the matching is case sensitive, and if `TRUE`, case is ignored.
+#' @param ignore.case `logical`; if `FALSE`, the matching is case sensitive, and if `TRUE` (the default), case is ignored.
 #' @param .context an optional character string providing context for error messages (e.g., a function or object name). Prepended to the error message when supplied before being evaluated by \pkg{cli}.
 #'
 #' @returns
-#' A character string (or vector, if `several.ok = TRUE`) of the matched element(s) from `choices`. If there is no match, an error is thrown. When `several.okay = TRUE`, no error is thrown if there is at least one match.
+#' A character string (or vector, if `several.ok = TRUE`) of the matched element(s) from `choices`. If there is no match, an error is thrown. When `several.ok = TRUE`, no error is thrown if there is at least one match.
 #'
 #' @details
 #' Partial matching is supported via [pmatch()]. If no match is found, an error is thrown listing the valid `choices`.
 #'
 #' Checks are run on `x` prior to matching: first, [arg_supplied()] is used to check whether `x` was supplied; then [arg_string()] (if `several.ok = TRUE`) or [arg_character()] (if `several.ok = FALSE`) is used to check whether `x` is a valid string or character vector, respectively.
+#'
+#' When `ignore.case = TRUE` (the default), an initial case-sensitive match is run, and if any values in `x` are unmatched, a second, case-insensitive match is run. When `ignore.case = FALSE`, only the first match is run. This ensures that exact matches (on both content and case) are prioritized before case-insensitive matches.
 #'
 #' @seealso
 #' * [match.arg()] for the base \R version
@@ -56,40 +58,36 @@
 #' try(h("Wrong")) # Error with context
 
 #' @export
-match_arg <- function(x, choices, several.ok = FALSE, ignore.case = FALSE,
-                      .context = NULL, .arg = rlang::caller_arg(x)) {
-  arg_supplied(choices)
-  arg_character(choices)
+match_arg <- function(x, choices, several.ok = FALSE, ignore.case = TRUE,
+                      .context = NULL, .arg = rlang::caller_arg(x), .call) {
+  arg_supplied(choices, .call = rlang::current_env())
+  arg_character(choices, .call = rlang::current_env())
 
-  arg_supplied(x, .arg = .arg)
+  arg_supplied(x, .arg = .arg, .call = .call)
 
   if (is_null(x)) {
     return(choices[1L])
   }
 
-  arg_flag(several.ok)
-  arg_flag(ignore.case)
-
-  .tr <- {
-    if (ignore.case) tolower
-    else identity
-  }
+  arg_flag(several.ok, .call = rlang::current_env())
+  arg_flag(ignore.case, .call = rlang::current_env())
 
   choices <- unique(choices)
 
   if (several.ok) {
-    arg_character(x, .arg = .arg)
+    arg_character(x, .arg = .arg, .call = .call)
   }
   else {
-    arg_string(x, .arg = .arg)
-
-    if (identical(.tr(x), .tr(choices))) {
-      return(choices[1L])
-    }
+    arg_string(x, .arg = .arg, .call = .call)
   }
 
-  i <- pmatch(.tr(x), .tr(choices), nomatch = 0L,
+  i <- pmatch(x, choices, nomatch = 0L,
               duplicates.ok = TRUE)
+
+  if (ignore.case && any(i == 0)) {
+    i[i == 0] <- pmatch(tolower(x[i == 0]), tolower(choices), nomatch = 0L,
+                        duplicates.ok = TRUE)
+  }
 
   if (all(i == 0L)) {
     one_of <- {
@@ -99,11 +97,13 @@ match_arg <- function(x, choices, several.ok = FALSE, ignore.case = FALSE,
     }
 
     if (is_null(.context)) {
-      err("{.arg {(.arg)}} should be {one_of} {.or {.val {choices}}}")
+      err("{.arg {(.arg)}} should be {one_of} {.or {.val {choices}}}",
+          .call = .call)
     }
     else {
       err(sprintf("%s {.arg {(.arg)}} should be {one_of} {.or {.val {choices}}}",
-                  .context))
+                  .context),
+          .call = .call)
     }
   }
 
